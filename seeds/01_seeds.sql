73236e03f58a6304908377d76eba35e90477e99f
gurpreet singh <gurpreet212004@gmail.com>
2021-08-18 23:24:39 +0000

    queries added

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -15,7 +15,7 @@ VALUES (1, 1, '2018-09-11', '2018-09-26'),
 (2, 2, '2019-01-04', '2019-02-01'),
 (3, 3, '2021-10-01', '2021-10-14');
 
-INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message_user)
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES (1, 1, 1, 3.5, 'message1'),
 (2, 3, 3, 3, 'message2'),
 (3, 3, 3, 4.5, 'message3');
